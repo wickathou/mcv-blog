3eb167a7845b5b9a0b99c7902f2f4b70fe8a285a
Javier <javidx@gmail.com>
2023-10-25 14:39:26 +0300

    Added indexes for foreign keys Renamed user_id to author_id on posts schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_25_083234) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_25_113002) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
