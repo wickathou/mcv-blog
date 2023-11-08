8d5c8a3cbd57de8930b252361e7f45ab55c1a5d8
Javier <javidx@gmail.com>
2023-11-08 13:36:31 +0200

    Added devise views Modified application controller to allow extra parameters for new users Added devise User model Modified new User registration view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: 'author_id'
-  has_many :likes, foreign_key: 'user_id'
-  has_many :comments, foreign_key: 'user_id'
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :likes, foreign_key: 'user_id', dependent: :destroy
+  has_many :comments, foreign_key: 'user_id', dependent: :destroy
   validates :name, presence: true, length: { minimum: 3, maximum: 20 }
   validates :posts_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :bio, length: { maximum: 250 }
+  validates :email, presence: true, uniqueness: true
 
   def last_three_posts
     posts.order(created_at: :desc).limit(3)
