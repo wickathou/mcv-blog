eb3e2930a13426939f0e6444104e8d8ef4654901
Javier <javidx@gmail.com>
2023-11-10 18:24:00 +0200

    Allowed app to return json Created alternative method to create comments through api requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery unless: -> { request.format.json? }
 
   # def after_sign_in_path_for(user)
   #   user_path(user)
